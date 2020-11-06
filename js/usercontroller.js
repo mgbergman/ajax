a38914dd306a247e5bd551b0f282d74d2ce9842b
Marc Bergman <mgbergman@gmail.com>
2020-11-06 15:16:33 -0500

    11/6 vol 2

--- js/usercontroller.js
+++ js/usercontroller.js
@@ -1,24 +1,41 @@
 $(() => {
 
-
-    const getAll = () => {
-        $.getJSON("http://localhost:8080/api/users/")
-            .done(users => {
-                console.log(users)
-                let tbody = $("#tbody")
-                for (let use of users) {
-                    let tr = $("<tr></tr>");
-                    let tduserName = $(`<td>${use.userName}</td>`);
-                    let tdfirstName = $(`<td>${use.firstName}</td>}`);
-                    let tdlastName = $(`<td>${use.lastName}</td>}`);
-                    tr.append(tduserName).append(tdfirstName).append(tdlastName);
-                    tbody.append(tr);
-
-                }
-
-          });
- 
-   }
-
-getAll(); 
+    $("#pkbutton").on("click", () => {
+        getOne();
+    });
 })
+//     const getAll = () => {
+//         $.getJSON("http://localhost:8080/api/users/")
+//             .done(users => {
+//                 console.log(users)
+//                 let tbody = $("#tbody")
+//                 for (let use of users) {
+//                     let tr = $("<tr></tr>");
+//                     let tduserName = $(`<td>${use.userName}</td>`);
+//                     let tdfirstName = $(`<td>${use.firstName}</td>}`);
+//                     let tdlastName = $(`<td>${use.lastName}</td>}`);
+//                     tr.append(tduserName).append(tdfirstName).append(tdlastName);
+//                     tbody.append(tr);
+
+//                 }
+
+//           });
+//  getAll(); 
+//    }
+
+const getOne = () => {
+    let id = $("#ppk").val();
+    console.log(id);
+       $.getJSON(`http://localhost:8080/api/users/${id}`)
+        .done(users => {
+        console.log(users);
+        $("#pid").val(users.id);
+        $("#puserName").val(users.userName);
+                
+
+            })
+          
+      };
+   
+
+
